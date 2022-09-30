e0e81ed43d08e9bd3d0ddf4901d7955e08f45b96
chubohao <bohao.chu@qq.com>
2022-09-30 16:02:09 +0200

    modified argumetns

--- source/arguments.py
+++ source/arguments.py
@@ -1,8 +1,10 @@
+####  SAMPLING INFORMATION ####
 # scene information
 dataset = 'CNC'    # scene
 activity = "off"   # activity
 duration = 10      # duration
 
+####  RUNING INFORMATION ####
 # raw data visualization server information
 url = 'http://192.168.0.104:8889/'
 headers = {'content-type': "application/json"}
